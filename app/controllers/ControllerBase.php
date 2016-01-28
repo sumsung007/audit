0ab7d8192602ddbc4490d107dd87a49532c88a7b
Joe <joe@xxtime.com>
2016-01-28 17:51:10 +0800

    基本设置

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -7,9 +7,24 @@ use Phalcon\Mvc\Controller;
 class ControllerBase extends Controller
 {
 
+    public $_config;
+    public $_app;
+
     public function initialize()
     {
+        global $config;
+        $this->_config = $config;
+        $this->_app = $this->dispatcher->getParam("app");
 
+
+        // 设置时区
+        ini_set("date.timezone", $this->_config->env->timezone);
+
+
+        // 日志记录
+        if ($config->development->record_request) {
+            write_log(urldecode(http_build_query($_REQUEST)), date("Ymd") . '.log');
+        }
     }
 
 }
